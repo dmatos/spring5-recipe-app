c1bdac8a56fdd986c2cee578496e5afec3e79d4f
dmatos <dmatos88@gmail.com>
2021-09-10 19:52:29 -0300

    oneToOne Ingredient UnityOfMeasure

---
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -0,0 +1,59 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnityOfMeasure unityOfMeasure;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public UnityOfMeasure getUnityOfMeasure() {
+        return unityOfMeasure;
+    }
+
+    public void setUnityOfMeasure(UnityOfMeasure unityOfMeasure) {
+        this.unityOfMeasure = unityOfMeasure;
+    }
+}
