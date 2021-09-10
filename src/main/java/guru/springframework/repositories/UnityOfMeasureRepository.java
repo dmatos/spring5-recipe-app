6616a8515022f39b61ac32983044b2b8ede51cfd
dmatos <dmatos88@gmail.com>
2021-09-10 20:23:42 -0300

    repositories

---
+++ src/main/java/guru/springframework/repositories/UnityOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import guru.springframework.model.UnityOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnityOfMeasureRepository extends CrudRepository<UnityOfMeasure, Long> {
+}
