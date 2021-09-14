b2e73755978ff7cf5d9a89f8e67d1ee0191d645b
dmatos <dmatos88@gmail.com>
2021-09-13 23:45:44 -0300

    no more gradle

--- src/test/java/guru/springframework/repositories/UnityOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnityOfMeasureRepositoryIT.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,7 +19,7 @@ import static org.junit.Assert.*;
 public class UnityOfMeasureRepositoryIT {
 
     @Autowired
-    UnityOfMeasureRepository unityOfMeasureRepository;
+    private UnityOfMeasureRepository unityOfMeasureRepository;
 
     @Before
     public void setUp() throws Exception{
